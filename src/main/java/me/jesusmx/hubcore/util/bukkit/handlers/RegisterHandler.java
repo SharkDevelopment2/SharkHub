ed8aee67db283773871b0609431564321f35c247
Miquel Angel <hulkrojo2@gmail.com>
2022-05-23 18:49:08 +0200

    changed some toggles

--- src/main/java/me/jesusmx/hubcore/util/bukkit/handlers/RegisterHandler.java
+++ src/main/java/me/jesusmx/hubcore/util/bukkit/handlers/RegisterHandler.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import me.jesusmx.hubcore.SharkHub;
 import me.jesusmx.hubcore.providers.ScoreboardProvider;
 import me.jesusmx.hubcore.providers.TablistProvider;
+import me.jesusmx.hubcore.util.files.ConfigFile;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 
@@ -14,28 +15,36 @@ public class RegisterHandler {
 
     @Getter private static Assemble assemble;
     @Getter private static Omega tablist;
+    private static final ConfigFile togglesConfig = SharkHub.getInstance().getTogglesConfig();
 
-    public static void registerProviders() {
+    public static void init() {
         registerScoreboard();
         registerTablist();
+        optimizeWorld();
     }
 
     public static void registerTablist() {
-        tablist = new Omega(SharkHub.getInstance(), new TablistProvider());
+        if (SharkHub.getInstance().getTablistConfig().getBoolean("TABLIST.ENABLE")) {
+            tablist = new Omega(SharkHub.getInstance(), new TablistProvider());
+        }
     }
 
     public static void registerScoreboard() {
-        assemble = new Assemble(SharkHub.getInstance(), new ScoreboardProvider());
-        assemble.setTicks(2);
-        assemble.setAssembleStyle(AssembleStyle.CUSTOM.descending(true).startNumber(16));
+        if (SharkHub.getInstance().getScoreboardConfig().getBoolean("SCOREBOARD.ENABLE")) {
+            assemble = new Assemble(SharkHub.getInstance(), new ScoreboardProvider());
+            assemble.setTicks(2);
+            assemble.setAssembleStyle(AssembleStyle.CUSTOM.descending(true).startNumber(16));
+        }
     }
 
-    public static void world() {
-        for (World world : Bukkit.getWorlds()) {
-            world.setGameRuleValue("doDaylightCycle", "false");
-            world.setGameRuleValue("doMobSpawning", "false");
-            world.setTime(0L);
-            world.setStorm(false);
+    public static void optimizeWorld() {
+        if (togglesConfig.getBoolean("world.optimized")) {
+            for (World world : Bukkit.getWorlds()) {
+                world.setGameRuleValue("doDaylightCycle", "false");
+                world.setGameRuleValue("doMobSpawning", "false");
+                world.setTime(0L);
+                world.setStorm(false);
+            }
         }
     }
 }
