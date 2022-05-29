5b0b318abc1f76c1d3b3a54b9fa17284c73310da
Miquel Angel <hulkrojo2@gmail.com>
2022-05-29 22:37:10 +0200

    fix visibility item

--- src/main/java/es/hulk/hub/hotbar/listeners/VisibilityToggleListener.java
+++ src/main/java/es/hulk/hub/hotbar/listeners/VisibilityToggleListener.java
@@ -38,14 +38,14 @@ public class VisibilityToggleListener implements Listener {
         if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
             if (player.getItemInHand().isSimilar(showItem)) {
                 for (Player online : Bukkit.getOnlinePlayers()) {
-                    player.showPlayer(online);
+                    player.hidePlayer(online);
                 }
 
                 player.sendMessage(CC.translate(messages.getString("PLAYER_VISIBILITY.SHOW")));
                 player.setItemInHand(hideItem);
             } else if (player.getItemInHand().isSimilar(hideItem)) {
                 for (Player online : Bukkit.getOnlinePlayers()) {
-                    player.hidePlayer(online);
+                    player.showPlayer(online);
                 }
 
                 player.sendMessage(CC.translate(messages.getString("PLAYER_VISIBILITY.HIDE")));
