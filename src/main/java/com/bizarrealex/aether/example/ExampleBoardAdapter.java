478dde1d26113b295b96b18edd079cb6c5dde802
Miquel Angel <hulkrojo2@gmail.com>
2022-06-18 13:18:41 +0200

    c

--- src/main/java/com/bizarrealex/aether/example/ExampleBoardAdapter.java
+++ src/main/java/com/bizarrealex/aether/example/ExampleBoardAdapter.java
@@ -21,70 +21,59 @@ import java.util.Set;
 
 public class ExampleBoardAdapter implements BoardAdapter, Listener {
 
-  public ExampleBoardAdapter(JavaPlugin plugin) {
-    Bukkit.getPluginManager().registerEvents(this, plugin);
-  }
-
-  @Override
-  public String getTitle(Player player) {
-    return "&6&lAether";
-  }
-
-  @Override
-  public List<String> getScoreboard(
-    Player player,
-    Board board,
-    Set<BoardCooldown> cooldowns
-  ) {
-    List<String> strings = new ArrayList<>();
-    strings.add("&7&m-------------------");
-
-    for (BoardCooldown cooldown : cooldowns) {
-      if (cooldown.getId().equals("enderpearl")) {
-        strings.add(
-          "&e&lEnderpearl&7:&c " + cooldown.getFormattedString(BoardFormat.SECONDS)
-        );
-      }
+    public ExampleBoardAdapter(JavaPlugin plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
-    strings.add("&7&m-------------------&r");
-
-    if (strings.size() == 2) {
-      return null;
+    @Override
+    public String getTitle(Player player) {
+        return "&6&lAether";
     }
 
-    return strings;
-  }
+    @Override
+    public List<String> getScoreboard(Player player, Board board, Set<BoardCooldown> cooldowns) {
+        List<String> strings = new ArrayList<>();
+        strings.add("&7&m-------------------");
 
-  @EventHandler
-  public void onPlayerInteractEvent(PlayerInteractEvent event) {
-    Player player = event.getPlayer();
-    Board board = Board.getByPlayer(player);
-    if (event.getAction().name().contains("RIGHT")) {
-      if (event.getItem() == null) return;
-      if (event.getItem().getType() != Material.ENDER_PEARL) return;
-      if (board == null) return;
-      if (player.getGameMode() == GameMode.CREATIVE) return;
+        for (BoardCooldown cooldown : cooldowns) {
+            if (cooldown.getId().equals("enderpearl")) {
+                strings.add("&e&lEnderpearl&7:&c " + cooldown.getFormattedString(BoardFormat.SECONDS));
+            }
+        }
 
-      if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-        event.setCancelled(true);
-        return;
-      }
+        strings.add("&7&m-------------------&r");
 
-      BoardCooldown cooldown = board.getCooldown("enderpearl");
-      if (cooldown != null) {
-        event.setCancelled(true);
-        player.updateInventory();
-        player.sendMessage(
-          ChatColor.RED +
-          "You must wait " +
-          cooldown.getFormattedString(BoardFormat.SECONDS) +
-          " seconds before enderpearling again!"
-        );
-        return;
-      }
+        if (strings.size() == 2) {
+            return null;
+        }
 
-      new BoardCooldown(board, "enderpearl", 16);
+        return strings;
     }
-  }
+
+    @EventHandler
+    public void onPlayerInteractEvent(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        Board board = Board.getByPlayer(player);
+        if (event.getAction().name().contains("RIGHT")) {
+            if (event.getItem() == null) return;
+            if (event.getItem().getType() != Material.ENDER_PEARL) return;
+            if (board == null) return;
+            if (player.getGameMode() == GameMode.CREATIVE) return;
+
+            if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                event.setCancelled(true);
+                return;
+            }
+
+            BoardCooldown cooldown = board.getCooldown("enderpearl");
+            if (cooldown != null) {
+                event.setCancelled(true);
+                player.updateInventory();
+                player.sendMessage(ChatColor.RED + "You must wait " + cooldown.getFormattedString(BoardFormat.SECONDS) + " seconds before enderpearling again!");
+                return;
+            }
+
+            new BoardCooldown(board, "enderpearl", 16);
+        }
+    }
 }
