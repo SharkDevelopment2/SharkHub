e71158a9264c365b13a8a0134750fdc152fce258
Miquel Angel <hulkrojo2@gmail.com>
2022-06-18 13:12:25 +0200

    a

--- src/main/java/com/bizarrealex/aether/Aether.java
+++ src/main/java/com/bizarrealex/aether/Aether.java
@@ -35,192 +35,195 @@ import static com.bizarrealex.aether.AetherOptions.defaultOptions;
 
 public class Aether implements Listener {
 
-  @Getter
-  private JavaPlugin plugin;
+    @Getter
+    private JavaPlugin plugin;
 
-  @Getter
-  private AetherOptions options;
+    @Getter
+    private AetherOptions options;
 
-  @Getter
-  BoardAdapter adapter;
+    @Getter
+    BoardAdapter adapter;
 
-  public Aether(JavaPlugin plugin, BoardAdapter adapter, AetherOptions options) {
-    this.options = options;
-    this.plugin = plugin;
+    public Aether(JavaPlugin plugin, BoardAdapter adapter, AetherOptions options) {
+        this.options = options;
+        this.plugin = plugin;
 
-    Bukkit.getPluginManager().registerEvents(this, plugin);
+        Bukkit.getPluginManager().registerEvents(this, plugin);
 
-    setAdapter(adapter);
-    run();
-  }
+        setAdapter(adapter);
+        run();
+        String serverVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+        System.out.println("[Aether] Registered Scoreboard Adapter for " + serverVersion);
+    }
 
-  public Aether(JavaPlugin plugin, BoardAdapter adapter) {
-    this(plugin, adapter, defaultOptions());
-  }
+    public Aether(JavaPlugin plugin, BoardAdapter adapter) {
+        this(plugin, adapter, defaultOptions());
+    }
 
-  public Aether(JavaPlugin plugin) {
-    this(plugin, null, defaultOptions());
-  }
+    public Aether(JavaPlugin plugin) {
+        this(plugin, null, defaultOptions());
+    }
 
-  private void run() {
-    new BukkitRunnable() {
-      @Override
-      public void run() {
-        if (adapter == null) return;
+    private void run() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (adapter == null) return;
 
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    Board board = Board.getByPlayer(player);
+                    if (board != null) {
+                        List<String> scores = adapter.getScoreboard(
+                                player,
+                                board,
+                                board.getCooldowns()
+                        );
+                        List<String> translatedScores = new ArrayList<>();
+
+                        if (scores == null) {
+                            if (!board.getEntries().isEmpty()) {
+                                for (BoardEntry boardEntry : board.getEntries()) {
+                                    boardEntry.remove();
+                                }
+
+                                board.getEntries().clear();
+                            }
+
+                            continue;
+                        }
+
+                        for (String line : scores) {
+                            translatedScores.add(ChatColor.translateAlternateColorCodes('&', line));
+                        }
+
+                        if (!options.scoreDirectionDown()) {
+                            Collections.reverse(scores);
+                        }
+
+                        Scoreboard scoreboard = board.getScoreboard();
+                        Objective objective = board.getObjective();
+
+                        if (!(objective.getDisplayName().equals(adapter.getTitle(player)))) {
+                            objective.setDisplayName(
+                                    ChatColor.translateAlternateColorCodes('&', adapter.getTitle(player))
+                            );
+                        }
+
+                        outer:
+                        for (int i = 0; i < scores.size(); i++) {
+                            String text = scores.get(i);
+                            int position;
+                            if (options.scoreDirectionDown()) {
+                                position = 15 - i;
+                            } else {
+                                position = i + 1;
+                            }
+
+                            Iterator<BoardEntry> iterator = new ArrayList<>(board.getEntries())
+                                    .iterator();
+                            while (iterator.hasNext()) {
+                                BoardEntry boardEntry = iterator.next();
+                                Score score = objective.getScore(boardEntry.getKey());
+
+                                if (
+                                        score != null &&
+                                                boardEntry
+                                                        .getText()
+                                                        .equals(ChatColor.translateAlternateColorCodes('&', text))
+                                ) {
+                                    if (score.getScore() == position) {
+                                        continue outer;
+                                    }
+                                }
+                            }
+
+                            int positionToSearch = options.scoreDirectionDown()
+                                    ? 15 - position
+                                    : position - 1;
+
+                            iterator = board.getEntries().iterator();
+                            while (iterator.hasNext()) {
+                                BoardEntry boardEntry = iterator.next();
+                                int entryPosition = scoreboard
+                                        .getObjective(DisplaySlot.SIDEBAR)
+                                        .getScore(boardEntry.getKey())
+                                        .getScore();
+
+                                if (!options.scoreDirectionDown()) {
+                                    if (entryPosition > scores.size()) {
+                                        iterator.remove();
+                                        boardEntry.remove();
+                                    }
+                                }
+                            }
+
+                            BoardEntry entry = board.getByPosition(positionToSearch);
+                            if (entry == null) {
+                                new BoardEntry(board, text).send(position);
+                            } else {
+                                entry.setText(text).setup().send(position);
+                            }
+
+                            if (board.getEntries().size() > scores.size()) {
+                                iterator = board.getEntries().iterator();
+                                while (iterator.hasNext()) {
+                                    BoardEntry boardEntry = iterator.next();
+                                    if (
+                                            (!translatedScores.contains(boardEntry.getText())) ||
+                                                    Collections.frequency(
+                                                            board.getBoardEntriesFormatted(),
+                                                            boardEntry.getText()
+                                                    ) >
+                                                            1
+                                    ) {
+                                        iterator.remove();
+                                        boardEntry.remove();
+                                    }
+                                }
+                            }
+                        }
+
+                        player.setScoreboard(scoreboard);
+                    }
+                }
+            }
+        }.runTaskTimerAsynchronously(plugin, 20L, 2L);
+    }
+
+    public void setAdapter(BoardAdapter adapter) {
+        this.adapter = adapter;
         for (Player player : Bukkit.getOnlinePlayers()) {
-          Board board = Board.getByPlayer(player);
-          if (board != null) {
-            List<String> scores = adapter.getScoreboard(
-              player,
-              board,
-              board.getCooldowns()
-            );
-            List<String> translatedScores = new ArrayList<>();
+            Board board = Board.getByPlayer(player);
 
-            if (scores == null) {
-              if (!board.getEntries().isEmpty()) {
-                for (BoardEntry boardEntry : board.getEntries()) {
-                  boardEntry.remove();
-                }
-
-                board.getEntries().clear();
-              }
-
-              continue;
+            if (board != null) {
+                Board.getBoards().remove(board);
             }
 
-            for (String line : scores) {
-              translatedScores.add(ChatColor.translateAlternateColorCodes('&', line));
-            }
-
-            if (!options.scoreDirectionDown()) {
-              Collections.reverse(scores);
-            }
-
-            Scoreboard scoreboard = board.getScoreboard();
-            Objective objective = board.getObjective();
-
-            if (!(objective.getDisplayName().equals(adapter.getTitle(player)))) {
-              objective.setDisplayName(
-                ChatColor.translateAlternateColorCodes('&', adapter.getTitle(player))
-              );
-            }
-
-            outer:for (int i = 0; i < scores.size(); i++) {
-              String text = scores.get(i);
-              int position;
-              if (options.scoreDirectionDown()) {
-                position = 15 - i;
-              } else {
-                position = i + 1;
-              }
-
-              Iterator<BoardEntry> iterator = new ArrayList<>(board.getEntries())
-                .iterator();
-              while (iterator.hasNext()) {
-                BoardEntry boardEntry = iterator.next();
-                Score score = objective.getScore(boardEntry.getKey());
-
-                if (
-                  score != null &&
-                  boardEntry
-                    .getText()
-                    .equals(ChatColor.translateAlternateColorCodes('&', text))
-                ) {
-                  if (score.getScore() == position) {
-                    continue outer;
-                  }
-                }
-              }
-
-              int positionToSearch = options.scoreDirectionDown()
-                ? 15 - position
-                : position - 1;
-
-              iterator = board.getEntries().iterator();
-              while (iterator.hasNext()) {
-                BoardEntry boardEntry = iterator.next();
-                int entryPosition = scoreboard
-                  .getObjective(DisplaySlot.SIDEBAR)
-                  .getScore(boardEntry.getKey())
-                  .getScore();
-
-                if (!options.scoreDirectionDown()) {
-                  if (entryPosition > scores.size()) {
-                    iterator.remove();
-                    boardEntry.remove();
-                  }
-                }
-              }
-
-              BoardEntry entry = board.getByPosition(positionToSearch);
-              if (entry == null) {
-                new BoardEntry(board, text).send(position);
-              } else {
-                entry.setText(text).setup().send(position);
-              }
-
-              if (board.getEntries().size() > scores.size()) {
-                iterator = board.getEntries().iterator();
-                while (iterator.hasNext()) {
-                  BoardEntry boardEntry = iterator.next();
-                  if (
-                    (!translatedScores.contains(boardEntry.getText())) ||
-                    Collections.frequency(
-                      board.getBoardEntriesFormatted(),
-                      boardEntry.getText()
-                    ) >
-                    1
-                  ) {
-                    iterator.remove();
-                    boardEntry.remove();
-                  }
-                }
-              }
-            }
-
-            player.setScoreboard(scoreboard);
-          }
+            Bukkit
+                    .getPluginManager()
+                    .callEvent(new BoardCreateEvent(new Board(player, this, options), player));
         }
-      }
-    }.runTaskTimerAsynchronously(plugin, 20L, 2L);
-  }
-
-  public void setAdapter(BoardAdapter adapter) {
-    this.adapter = adapter;
-    for (Player player : Bukkit.getOnlinePlayers()) {
-      Board board = Board.getByPlayer(player);
-
-      if (board != null) {
-        Board.getBoards().remove(board);
-      }
-
-      Bukkit
-        .getPluginManager()
-        .callEvent(new BoardCreateEvent(new Board(player, this, options), player));
     }
-  }
 
-  @EventHandler(priority = EventPriority.MONITOR)
-  public void onPlayerJoinEvent(PlayerJoinEvent event) {
-    if (Board.getByPlayer(event.getPlayer()) == null) {
-      Bukkit
-        .getPluginManager()
-        .callEvent(
-          new BoardCreateEvent(
-            new Board(event.getPlayer(), this, options),
-            event.getPlayer()
-          )
-        );
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        if (Board.getByPlayer(event.getPlayer()) == null) {
+            Bukkit
+                    .getPluginManager()
+                    .callEvent(
+                            new BoardCreateEvent(
+                                    new Board(event.getPlayer(), this, options),
+                                    event.getPlayer()
+                            )
+                    );
+        }
     }
-  }
 
-  @EventHandler(priority = EventPriority.HIGHEST)
-  public void onPlayerQuitEvent(PlayerQuitEvent event) {
-    Board board = Board.getByPlayer(event.getPlayer());
-    if (board != null) {
-      Board.getBoards().remove(board);
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerQuitEvent(PlayerQuitEvent event) {
+        Board board = Board.getByPlayer(event.getPlayer());
+        if (board != null) {
+            Board.getBoards().remove(board);
+        }
     }
-  }
 }
